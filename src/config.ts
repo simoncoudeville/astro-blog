d03a308f83eaae30325cc8ec22be88beed347fda
Simon <coudeville.simon@gmail.com>
2022-12-08 22:49:58 +0100

    Finish it!

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,17 @@
 export const SITE_ATHOUR = 'Simon Coudeville';
 
 // a function tho set the dates to .toLocaleDateString("en-gb").replace(/\//g, "-")
+// export const formatDate = (date: string | number | Date) => {
+//     return new Date(date).toLocaleDateString("en-gb").replace(/\//g, " - ");
+// };
+
+// a function to set the dates to .toLocaleTimeString("en-gb") with options
 export const formatDate = (date: string | number | Date) => {
-    return new Date(date).toLocaleDateString("en-gb").replace(/\//g, " - ");
+    return new Date(date).toLocaleDateString("en-gb", {
+        year: "numeric",
+        day: "numeric",
+        month: "long"
+    });
 };
 
 export default formatDate;
