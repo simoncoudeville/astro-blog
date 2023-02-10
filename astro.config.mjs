a6a55549cc308073ba76d12c1319048afbe5397c
Simon <coudeville.simon@gmail.com>
2023-02-10 09:28:59 +0100

    Fixed some rebase stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import preact from "@astrojs/preact";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), mdx({
+  integrations: [mdx({
     extendPlugins: 'defaults',
     extendMarkdownConfig: false,
     smartypants: true,
-    gfm: true
-  }), svelte()],
-  markdown: {
+    gfm: true,
     syntaxHighlight: 'prism'
-  }
+  }), svelte()],
 });
